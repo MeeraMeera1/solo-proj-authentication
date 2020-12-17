ccea6c5cdc032ac35f76537841cda4f45dafd95d
MeeraMeera1 <gatesameera98@gmail.com>
2020-12-17 15:48:19 -0500

    Set up error handlers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes/index')
 const { enviornment } = require('./config/index');
+const { ValidationError } = require('sequelize');
 
 const isProduction = enviornment === 'production';
 
@@ -37,5 +38,36 @@ app.use(
 );
 
 app.use(routes);
+
+//catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ("The requested resource couldn't be found.");
+    err.status = 404;
+    next(err);
+});
+
+//process sequelize errors
+app.use((err,_req,_res, next) => {
+    if(err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+//error formatter 
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
+
 
 module.exports = app;
